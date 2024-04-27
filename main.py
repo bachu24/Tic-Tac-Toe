998a92e036413837ee1d2164e07fcbe6584b3b34
bachu24 <peldenchewang2003@gmail.com>
2024-04-28 02:15:03 +0700

    icons placement

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import pygame
 
 # pygame setups
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+WINDOW_WIDTH = 800 
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_WIDTH))
 clock = pygame.time.Clock()
 running = True
 
+def load_icons(path,resolution):
+    icon = pygame.image.load(path)
+    return pygame.transform.scale(icon, resolution)
+    
+grid=load_icons('img/grid.png',[WINDOW_WIDTH,WINDOW_WIDTH])
+iconX=load_icons('img/x-icon.png',[WINDOW_WIDTH//3,WINDOW_WIDTH//3])
+iconO=load_icons('img/o-icon.png',[WINDOW_WIDTH//3,WINDOW_WIDTH//3])
+ 
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -15,11 +24,16 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("white")
 
     # RENDER YOUR GAME HERE
 
     # flip() the display to put your work on screen
+    
+    screen.blit(grid,(0,0)) 
+    screen.blit(iconX,(0,0))
+    screen.blit(iconO,(WINDOW_WIDTH//3,WINDOW_WIDTH//3))
+    
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
