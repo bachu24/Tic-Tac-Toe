812fe8863c985984a30e99488b22494cf67d717c
bachu24 <peldenchewang2003@gmail.com>
2024-04-29 14:51:39 +0700

    draw icons

--- main.py
+++ main.py
@@ -27,9 +27,18 @@ player = 0
 def play_turn(current_player):
      current_coordinate = pygame.math.Vector2(pygame.mouse.get_pos())//PIXEL_WIDTH 
      if  pygame.mouse.get_pressed()[0]:
-         board[current_coordinate.x][current_coordinate.y] = 0 if current_player == 0 else 1
-         global player
-         player = 1 - player;
+        col, row = map(int, current_coordinate)
+        board[row][col] = current_player
+        global player
+        player = 1 - player
+         
+def draw_icons():
+    for i,row in enumerate(board):
+        for j,col in enumerate(board[i]):
+            if board[i][j] == 0:
+                screen.blit(iconX,(j*PIXEL_WIDTH,i*PIXEL_WIDTH))
+            elif board[i][j] == 1:
+                screen.blit(iconO,(j*PIXEL_WIDTH,i*PIXEL_WIDTH))
          
 while running:
     # poll for events
@@ -39,18 +48,18 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("white")
-
     # RENDER YOUR GAME HERE
-
     # flip() the display to put your work on screen
+    pygame.display.flip()
+    screen.fill("white")
     
     screen.blit(grid,(0,0)) 
-    screen.blit(iconX,(0,0))
-    screen.blit(iconO,(WINDOW_WIDTH//3,WINDOW_WIDTH//3))
-    
-    pygame.display.flip()
-
+    pygame.event.wait()
+    play_turn(player)
+    draw_icons()
     clock.tick(60)  # limits FPS to 60
 
+    # screen.blit(iconX,(0,0))
+    # screen.blit(iconO,(WINDOW_WIDTH//3,WINDOW_WIDTH//3))
+    
 pygame.quit()
